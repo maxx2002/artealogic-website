2695c2b3829300dd9929ad9cc500fd4f6345cd80
Maximus Aurelius Wiranata <83909501+maxx2002@users.noreply.github.com>
2023-12-09 15:24:12 +0700

    Added 3 perspectives

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,17 @@ burger.addEventListener("click", () => {
         menu.classList.add("hidden");
     }
 });
+
+window.addEventListener('load', () => {
+    revealFunction();
+})
+
+function revealFunction(){
+    window.sr = ScrollReveal({ duration:1000, distance:'50px', easing:'ease-out'});
+
+    sr.reveal('.reveal-left', {origin:'left', reset:false});
+    sr.reveal('.reveal-top', {origin:'top', reset:false});
+    sr.reveal('.reveal-bottom', {origin:'bottom', reset:false});
+    sr.reveal('.reveal-right', {origin:'right', reset:false});
+    sr.reveal('.reveal-rotate', {origin:'bottom', rotate:{x:1000, z:1000}, reset:false});
+}
