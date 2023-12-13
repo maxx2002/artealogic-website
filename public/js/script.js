e6fc445c33cf2efcc43c3b7d5fe9da83bd7fab04
Maximus Aurelius Wiranata <83909501+maxx2002@users.noreply.github.com>
2023-12-13 18:57:45 +0700

    Added whatsapp and email link

--- public/js/script.js
+++ public/js/script.js
@@ -9,16 +9,16 @@ burger.addEventListener("click", () => {
     }
 });
 
-window.addEventListener('load', () => {
-    revealFunction();
-})
+// window.addEventListener('load', () => {
+//     revealFunction();
+// })
 
-function revealFunction(){
-    window.sr = ScrollReveal({ duration:1000, distance:'50px', easing:'ease-out'});
+// function revealFunction(){
+//     window.sr = ScrollReveal({ duration:1000, distance:'50px', easing:'ease-out'});
 
-    sr.reveal('.reveal-left', {origin:'left', reset:false});
-    sr.reveal('.reveal-top', {origin:'top', reset:false});
-    sr.reveal('.reveal-bottom', {origin:'bottom', reset:false});
-    sr.reveal('.reveal-right', {origin:'right', reset:false});
-    sr.reveal('.reveal-rotate', {origin:'bottom', rotate:{x:1000, z:1000}, reset:false});
-}
+//     sr.reveal('.reveal-left', {origin:'left', reset:false});
+//     sr.reveal('.reveal-top', {origin:'top', reset:false});
+//     sr.reveal('.reveal-bottom', {origin:'bottom', reset:false});
+//     sr.reveal('.reveal-right', {origin:'right', reset:false});
+//     sr.reveal('.reveal-rotate', {origin:'bottom', rotate:{x:1000, z:1000}, reset:false});
+// }
